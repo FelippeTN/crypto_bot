e76b3e054a2f681fa346a16c15576da4dd58f206
FelippeTN <felippenalim2004@gmail.com>
2025-01-30 16:36:31 -0300

    UPDATE: Expecificação do tipo de resposta de acordo com a mensagem

--- app/bot.py
+++ app/bot.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 from tools.groq_config import groq_chat
 from tools.fetch_crypto import *
 from config.log_config import logger
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
-    CallbackQueryHandler,
     CallbackContext,
     MessageHandler,
     filters,
@@ -15,72 +14,44 @@ from telegram.ext import (
 
 # Carregar variáveis de ambiente
 load_dotenv()
-
-
 API_KEY = os.getenv('API_KEY')
 
 class TelegramBot:
     def __init__(self):
         logger.info("Inicializando o TelegramChatbot")
-        self.default_message_reply = "Olá! Gostaria de conversar com Nalim's BOT?"
         self.TELEGRAM_TOKEN = API_KEY
         self.esperando_resposta = False
+        self.file_path = "./data/crypto_keywords.txt"
 
     def start_message(self):
-        logger.info("Menu loaded...")
-        try:
-            menu = self.default_message_reply
-            return menu
-        
-        except Exception as e:
-            logger.error(f"Erro ao carregar o menu: {e}")
-            raise
-            
-    def generate_keyboard(self, menu, prefix_menu="submenu_", prefix_action="action_"):
-        logger.info("Gerando teclado inline")
-        keyboard = [
-            [InlineKeyboardButton("Sim", callback_data=f"{prefix_action}1")],
-            [InlineKeyboardButton("Não", callback_data=f"{prefix_action}2")],
-        ]
-        logger.info("Teclado inline gerado com sucesso")
-        return keyboard
-    
-    async def button(self, update: Update, context: CallbackContext):
-        query = update.callback_query
-        await query.answer()
+        logger.info("Mensagem inicial carregada...")
+        return self.default_message_reply
 
-        # Condicional baseado na escolha do usuário
-        if query.data == "action_1":
-            response_text = """Ok, caso queira sair da conversa, é só digitar "sair"!\nFaça sua pergunta:"""
-            self.esperando_resposta = True
-        elif query.data == "action_2":
-            response_text = "OK! Quando precisar de mim, estarei à disposição!"
-        else:
-            response_text = "Opção desconhecida."
-        
-        logger.info('Opção selecionada pelo usuário')
-        # Edita a mensagem original com a resposta
-        await query.edit_message_text(text=response_text)
-        
     async def handle_message(self, update: Update, context: CallbackContext):
         user_message = update.message.text.lower()
-        #recommendations = rank_assets_with_qwen(crypto_assets)
+        chat_type = update.message.chat.type
+        crypto_assets = ["BTC-USD", "ETH-USD"]
         
-        if not self.esperando_resposta:
-            # Inicia a conversa com a mensagem padrão e teclado
-            menu = self.start_message()
-            keyboard = self.generate_keyboard(menu)
-            await update.message.reply_text(
-                text=menu, reply_markup=InlineKeyboardMarkup(keyboard)
-            )
-        elif user_message != "sair":
+        with open(self.file_path, "r") as file:
+            crypto_content = file.read()
+
+        if chat_type in ["group", "supergroup"] and f"@{context.bot.username}" not in user_message:
+            return
+
+        if any(word in user_message for word in crypto_content.split()):
             logger.info('Groq está sendo chamado...')
-            response_text = groq_chat(user_message)
+            analyze_crypto = rank_assets_with_qwen(crypto_assets)
+            response_text = groq_chat(analyze_crypto)
             await update.message.reply_text(text=response_text)
         else:
-            self.esperando_resposta = False
-            await update.message.reply_text(text="Encerrando a conversa. Até logo!")
-            
+            response_text = groq_chat(user_message)
+            await update.message.reply_text(text=response_text)
+
+    async def start_command(self, update: Update, context: CallbackContext):
+        """Responde ao comando /start sem teclado"""
+        menu = self.start_message()
+        await update.message.reply_text(text=menu)
+
     def start_bot(self):
         try:
             logger.info("Inicializando a aplicação do bot")
@@ -89,20 +60,17 @@ class TelegramBot:
                 .token(self.TELEGRAM_TOKEN)
                 .pool_timeout(20)
                 .connect_timeout(15)
-                .read_timeout(60)  # Aumentar o tempo de leitura
-                .write_timeout(60)  # Aumentar o tempo de escrita
+                .read_timeout(60)
+                .write_timeout(60)
                 .build()
             )
+
+            start_handler = CommandHandler("start", self.start_command)
+            application.add_handler(start_handler)
             
-            # Handler para lidar com as respostas do teclado inline
-            button_handler = CallbackQueryHandler(self.button)
-            application.add_handler(button_handler)
-            
-            # Handler para lidar com todas as mensagens de texto
             message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), self.handle_message)
             application.add_handler(message_handler)
             
-            # Iniciar o bot
             application.run_polling()
             
         except Exception as e:
